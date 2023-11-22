8c9e718c35c36f0e51aa4016cfdc5e4e3f6c9d1a
GioxBRUSH <105091966+GioxBRUSH@users.noreply.github.com>
2023-11-22 14:42:25 -0300

    commit RU 14:42

---
+++ backend_laravel/app/Http/Controllers/ProductController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use App\Http\Requests\StoreProductRequest;
+use App\Http\Requests\UpdateProductRequest;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $products = Product::all();
+        return $products;
+    }
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreProductRequest $request)
+    {
+       $product = new Product();
+       $product->name = $request->name;
+       $product->description = $request->description;
+       $product->price = $request->price;
+       $product->image = $request->image;
+       
+
+       $product->save();
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateProductRequest $request, Product $product)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Product $product)
+    {
+        //
+    }
+}
